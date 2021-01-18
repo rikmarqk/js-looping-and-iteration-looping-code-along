5dc2327d21b3551050f86af09a6d4caa6b813822
rikmarqk <rikmarqk@gmail.com>
2021-01-18 16:05:11 -0600

    commit tree 58a984702efce64687c7db0c6ec240b97283af6a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+names(["Ada","Brendan","Ali"], "birthday")
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log('Thank you, ${names[0][i]} for a wonderful ${names[1]} gift!');
+    }
+    return names
+}
